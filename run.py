df90956178468ee55d44a7874672233fe668b6ab
aidankahng <aidankahng@gmail.com>
2024-03-08 09:44:32 -0800

    changed messaging and spacing

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ def main():
     print("Welcome to the CRUD checklist!")
     print("The checklist is currently empty.")
     while True:
-        choice = input("Choose one of the following:\n1. Add Item\n2. View All\n3. View One Item\n4. Mark Completed\n5. Mark Incomplete\n6. Edit Item\n7. Delete Item\n8. Quit Program\n")
+        choice = input("\nChoose one of the following:\n1. Add Item\n2. View All\n3. View One Item\n4. Mark Completed\n5. Mark Incomplete\n6. Edit Item\n7. Delete Item\n8. Quit Program\n")
         while choice not in {'1','2','3','4','5','6','7','8'}:
             choice = input("Please pick a valid option: ")
         if choice == '8':
@@ -24,8 +24,8 @@ def main():
             c.edit_item()
         elif choice == '7':
             c.delete_item()
-    print("Thank you for using the checklist.")
-    print("This was your final checklist:")
+    print("Thank you for using the crud checklist.")
+    print("This was the final checklist:")
     print(c)
 
 main()
